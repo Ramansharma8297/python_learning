8d8b744e4c26afd8599108da9d978897bf028a92
raman <ramansharma8297@gmail.com>
2025-05-07 22:22:26 -0400

    yuwg

--- hello.py
+++ hello.py
@@ -1,3 +1,8 @@
 # This is a simple Python program to print "Hello, World!"
 
 print("Hello, World!")
+# This program demonstrates the basic syntax of Python and how to print output to the console.
+# It is a common first program for beginners learning Python.
+# The print function is used to display the string "Hello, World!" on the screen.
+# This program does not take any input or perform any calculations.
+# It simply serves as an introduction to the Python programming language.
